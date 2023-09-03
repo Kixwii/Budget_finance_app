8a386879f27936cfc1302038278c8b1328a5b40c
Kixwii <ryankiswii_2025@depauw.edu>
2023-09-03 17:05:34 -0400

    modified:   finance_app/src/App.css 	modified:   finance_app/src/components/Budget.js

--- finance_app/src/components/Budget.js
+++ finance_app/src/components/Budget.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
 const Budget =({amount, period, progress}) => {
+
+    const progressBar = '${progress}%';
     return (
         <div className="budget">
             <h2>{period}</h2>
             <p>Budget Amount: ${amount}</p>
-            <p>Progress: {progress}%</p>
-
+            <div className="progress-bar">
+                <div className="progress" style={{width: progressBar}}>
+                    <div className="slider"></div>
+                </div>
+            </div>
+           
         </div>
     );
 };
